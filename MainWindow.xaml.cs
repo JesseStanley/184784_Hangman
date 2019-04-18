d3172c7d6d0fe92a41392786cfcaa918e7c3cffc
JesseStanley <47399968+JesseStanley@users.noreply.github.com>
2019-04-18 08:59:28 -0400

    Added files

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿/* Jesse Stanley
+ * 4/16/2019
+ * Hangman Project
+ * to create a working hangman game
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _184784_Hangman
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnNewGame_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
